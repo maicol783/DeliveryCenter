ff09f03330afdf230e2139375ce4e25839127245
maicol783 <maicolocampoareiza783@gmail.com>
2021-03-11 23:29:06 -0500

    Tablas creada, no todas tienen campos

---
+++ database/migrations/2021_03_10_004904_create_sedes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSedesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sedes', function (Blueprint $table) {
+            $table->increments('id_sede');
+            $table->string('nombre_sede',35);
+            $table->string('direccion_sede',35);
+            $table->integer('id_barrio')->unsigned();
+            $table->foreign('id_barrio')->references('id_barrio')->on('barrios');
+            $table->integer('id_municipio')->unsigned();
+            $table->foreign('id_municipio')->references('id_municipio')->on('municipios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sedes');
+    }
+}
--- database/migrations/2021_03_11_180212_create_empleados_table.php
+++ database/migrations/2021_03_11_180212_create_empleados_table.php
@@ -14,7 +14,7 @@ class CreateEmpleadosTable extends Migration
     public function up()
     {
         Schema::create('empleados', function (Blueprint $table) {
-            $table->string('documento',20);
+            $table->string('documento',20)->primary();
             $table->string('primer_nombre',20);
             $table->string('segundo_nombre',20);
             $table->string('primer_apellido',20);
@@ -24,8 +24,10 @@ class CreateEmpleadosTable extends Migration
             $table->string('correo',45)->unique();
             $table->string('contraseña',30);
             $table->string('grupo_sanguineo',10);
-            $table->integer('id_sede');
-            $table->integer('id_rol');
+            $table->integer('id_sede')->unsigned();
+            $table->foreign('id_sede')->references('id_sede')->on('sedes');
+            $table->integer('id_rol')->unsigned();
+            $table->foreign('id_rol')->references('id_rol')->on('roles');
             $table->timestamps();
         });
     }
